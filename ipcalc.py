5b0ddffbde6a174a2c880272c1909c36e5034805
rasm033d <54931680+rasm033d@users.noreply.github.com>
2019-10-22 22:15:32 +0200

    Update ipcalc.py

--- ipcalc.py
+++ ipcalc.py
@@ -2,15 +2,22 @@
 import random
 import ipaddress
 from ipaddress import IPv4Network
-#List of subnets, in the way of /X
+
 SubList = ["14", "15", "16", "17", "18", "19", "20"]
-#Generates a random string of 4x 0-255
+#Just for now
 ip = ".".join(map(str, (random.randint(0, 255) 
                         for _ in range(4))))
-#Assings a subnet to the generated IP
+#IPfirst = IPv4Address(ip) #Marks the random IP as an IP address
 subnet = ip + "/" + random.choice(SubList)
-#Marks the IP and subnet as a network
-Network = IPv4Network(subnet, False)
+Network = IPv4Network(subnet, False) #Adds the IP to a subnet mask
 
 #Prints the network
 print(Network)
+print(Network.broadcast_address) #prints the broadcast address for the network, here for testing purposes
+
+#Asks the user for input
+Broadcast=str(input("Please input the broadcast address for the network "))
+
+#Checks if the address is the broadcast address:
+if Broadcast == Network.broadcast_address:
+    print("Good job!")
