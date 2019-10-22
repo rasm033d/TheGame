9af4ed60eca04f8154bae05d7daefb7e7a3e320f
rasm033d <54931680+rasm033d@users.noreply.github.com>
2019-10-22 22:08:34 +0200

    Update ipcalc.py

--- ipcalc.py
+++ ipcalc.py
@@ -1,9 +1,16 @@
+#Imports the necessary functions from libraries
 import random
 import ipaddress
-
+from ipaddress import IPv4Network
+#List of subnets, in the way of /X
+SubList = ["14", "15", "16", "17", "18", "19", "20"]
+#Generates a random string of 4x 0-255
 ip = ".".join(map(str, (random.randint(0, 255) 
                         for _ in range(4))))
-network = str(ip + "/24")
-print(network)
+#Assings a subnet to the generated IP
+subnet = ip + "/" + random.choice(SubList)
+#Marks the IP and subnet as a network
+Network = IPv4Network(subnet, False)
 
-broadcast = str(input("Enter the host IP ")
+#Prints the network
+print(Network)
