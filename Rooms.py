02f15ef1f41518862486d535b229361500101c96
rasm033d <54931680+rasm033d@users.noreply.github.com>
2019-10-28 14:45:40 +0100

    NEWNEWNEWN

--- Rooms.py
+++ Rooms.py
@@ -1,7 +1,3 @@
-#https://python-forum.io/Thread-Text-Adventure-Tutorial-if-structure-to-dictionary
-#Skeleton structure of the way the rooms will work, i.e. where the games are.
-#TODO: Add enough rooms for all the games, find a way to visually represent where the player is in relation to the world map, fix the player representation on the map, and add more
-from ipcalc import winconIP
 """
 if_adventure.py
  
@@ -9,33 +5,29 @@ A text adventure using if/elif statements.
 """
  
 # intro/setup
-print("Welcome to Group 5's game!")
+print("Welcome to Ichabod's Text Adventure")
 current_room = 'start'
-
-if current_room == 'start':
-    print("|[ ][ ][ ]|")
-    print("|[ ][ ][ ]|")
-    print("|[ ][^][ ]|")
-    print('This is the start room!')
-    print("This is where you will be able to decide where you go")
-    answerkey = input("Do you want to play our game? Press E ")
-    if answerkey == 'e':
-        current_room = "Math"
-elif current_room == 'Math':
-        print("|[ ][ ][ ]|")
-        print("|[ ][^][ ]|")
-        print("|[ ][ ][ ]|")
-        print('This is where the math game will be')
-        print('Need to figure out how to load it into this')
-elif current_room == 'hangman':
-        print("|[ ][^][ ]|")
-        print("|[ ][ ][ ]|")
-        print("|[ ][ ][ ]|")
-        print('You are in the hangman chamber.')
-        print('Load it in')
-elif current_room == 'subnet':
-        print("|[ ][ ][^]|")
-        print("|[ ][ ][ ]|")
-        print("|[ ][ ][ ]|")
-        print('Subnet challenge')
-        print('Load it in')
+ 
+# game loop
+while True:
+    command = input('\nPress E to continue ')
+    # display room contents
+    print()
+    if current_room == 'start':
+        print('Hello and welcome to our game!')
+        print('This is where you set off on our epic adventure!')
+        if command == 'e':
+            current_room = 'temple'
+    elif current_room == 'temple':
+        print('Welcome to our math game!')
+        print('This is a test of your aptitude in mathematics!')
+        
+        if command == 'e'
+            current_room = 
+    elif current_room == 'torture':
+        print('You are in a torture chamber.')
+        print('There is a rack and an iron maiden against the wall')
+        print('and some chains and thumbscrews on the floor.')
+    elif current_room == 'bedroom':
+        print('You are in a bedroom.')
+        print('There is a large bed with black, silk sheets on it.')
